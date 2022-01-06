da9900cae2140d4c2f7a51f8b821709fdf76cb3f
akihiro.tanaka <big.a.0816@gmail.com>
2022-01-06 15:05:35 +0900

    refactor folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "oak_decorators";
 import { AppController } from "./app.controller.ts";
-import { AnalysisModule } from "./api/analysis/analysis.module.ts";
-import { SamplesModule } from "./api/sample/sample.module.ts";
+import { AnalysisModule } from "./analysis/analysis.module.ts";
+import { SamplesModule } from "./sample/sample.module.ts";
 
 @Module({
   modules: [AnalysisModule, SamplesModule],
